ab653165be0bcc61baf153b2ec2c5aaee38f4e5d
MartaGaio14 <gaio.marta@gmail.com>
2020-05-26 08:25:43 +0100

    programma dopo la chiamata del 25 magg

--- project.py
+++ project.py
@@ -3,14 +3,7 @@ import ssl
 from collections import Counter #utile per il dizionario
 import numpy as np
 
-PATH=r"ch2.txt"
 
-# CARICAMENTO TXT IN FORMATO STRINGA
-f = open(PATH,encoding="utf8")
-raw = f.read()
-
-# FIRST STEP, TOKENIZATION, namely the process of breaking down a text paragraph into smaller chunks
-# What's a token?  Token is a single entity that is building blocks for sentence or paragraph.
 try:
     _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
@@ -19,20 +12,10 @@ else:
     ssl._create_default_https_context = _create_unverified_https_context
 nltk.download("punkt")
 
-# WORD TOKENIZATION
-from nltk.tokenize import word_tokenize
-tokenized_word=word_tokenize(raw)
 
-# Frequency distribution
-from nltk.probability import FreqDist
-import matplotlib.pyplot as plt
-
-# fdist = FreqDist(tokenized_word)          # SAREBBE BELLO FAR VEDERE UNA EVOLUZIONE GRAFICA DELLA NOSTRA PULIZIA
-# fdist.plot(30,cumulative=False)
-# plt.show()
-
-####DEFINIAMO LA LISTA DELLE STOPWORDS
-# Stopwords considered as noise in the text. Text may contain stop words such as is, am, are, this, a, an, the, etc.
+##################DEFINIAMO LA LISTA DELLE STOPWORDS
+# Stopwords considered as noise in the text. Text may 
+#contain stop words such as is, am, are, this, a, an, the, etc.
 
 from nltk.corpus import stopwords
 nltk.download('stopwords')
@@ -47,28 +30,35 @@ for i in range(0,25):
     stop_words.add(lettere[i])
 for i in range(0,10):
     stop_words.add(numeri[i])
+    
+len(stop_words)
+type(stop_words)
 
-# REMOVING STOPWORDS
-text1 = word_tokenize(raw.lower())
-stopwords = [x for x in text1 if x not in stop_words ]
+stop_words.add("oo")
+stop_words.add("ooo")
+stop_words.add("oooo")
+stop_words.add("ooooo")
+stop_words.add("oooooo")
+stop_words.add("ooooooo")
+stop_words.add("oooooooo")
+stop_words.add("ooooooooo")
+stop_words.add("pr")
+stop_words.add("fr")
+stop_words.add("f0")
+stop_words.add("f1")
+stop_words.add("f2")
+stop_words.add("pn")
+stop_words.add("et")
+stop_words.add("al")
+stop_words.add("panel")
+stop_words.add("show")
 
-# PULISCO INOLTRE DALLA PUNTEGGIATURA
-words_nopunct= [word for word in stopwords if word.isalnum()]
 
-len(words_nopunct) #5609
-# Come si puo vedere, si passa da 35528 token, a 5609
 
-# Rivediamo graficamente ora i risultati dopo questa prima pulizia
-# fdist = FreqDist(stopwords)
-# fdist.plot(30,cumulative=False)
-# plt.show()
-
-###TAGGING
+#######################################TAGGING
 
 nltk.download('averaged_perceptron_tagger')
-print("Parts of speech:", nltk.pos_tag(words_nopunct))
-tagged_words=nltk.pos_tag(words_nopunct)
-type(tagged_words) #lista di tuple ('parola', 'tag')
+
 
 #lista di tag riferita a parole che vogliamo rimuovere
 REM=['CC','CD','DT','IN','MD','NNP','NNPS','PRP','PSRP$','RB','RBR','RBS','TO','WDT','WPD$','WRB']
@@ -89,73 +79,92 @@ def eliminare(tagged_words1):
     return togli
 
 
-togli=eliminare(tagged_words)
-len(togli) #numero di elementi in totale
-sum(togli) #numero di elementi da eliminare: 875
+######################################àLEMMAIZATION:
+# Lemmatization reduces words to their base word, which is linguistically correct lemmas.
+# Lemmatization is usually more sophisticated than stemming.
+# Stemmer works on an individual word without knowledge of the context. For example, The word "better" has "good" as its lemma.
+# This thing will miss by stemming because it requires a dictionary look-up.
 
-
-## creiamo l'array di indici corrispondenti alle posizioni di '1' in togli
-
-indici=np.zeros(int(sum(togli))) #inizializzoamo
-
-for i in np.arange(0, 100):
-    print(togli[i])
-
-togli= np.array(togli, dtype=int)
-finali=list(np.array(words_nopunct)[togli==0])
-len(finali) #4734
+from nltk.stem.wordnet import WordNetLemmatizer
+nltk.download('wordnet')
+lem = WordNetLemmatizer()
 
 
 
-###
-finali = words_nopunct
-###
+#######################CERCHIAMO I BIGRAMMI IMPORTANTI PER OGNI CAPITOLO
 
 
+import tqdm
 
+PATH=["ch2.txt", "ch3.txt", "ch4.txt", "ch5.txt","ch6.txt","ch7.txt",
+       "ch8.txt","ch9.txt","ch10.txt","ch11.txt","ch12.txt","ch13.txt",
+        "ch14.txt", "ch15.txt","ch16.txt","ch17.txt","ch18.txt"]
 
-#visualizziamo i risultati dopo la pulizia finale
-# fdist = FreqDist(finali)
-# fdist.plot(30,cumulative=False)
-# plt.show()
+d=[]
+#len(PATH)-1)
+for i in tqdm.tqdm(range(0, len(PATH))):
+    perc=PATH[i]
+    f = open(perc,encoding="utf8")
+    raw = f.read()
+    #############################PULIZIA
+    #TOKENIZATION
+    tokenized_word=word_tokenize(raw) 
+    text1 = word_tokenize(raw.lower())
+    #RIMOZIONE DELLE STOPWORDS
+    stopwords = [x for x in text1 if x not in stop_words ] 
+    #RIMOZIONE DELLA PUNTEGGIATURA
+    words_nopunct= [word for word in stopwords if word.isalnum()] 
+    #TAGGING
+    tagged_words=nltk.pos_tag(words_nopunct) 
+    togli=eliminare(tagged_words)
+    indici=np.zeros(int(sum(togli))) 
+    togli= np.array(togli, dtype=int)
+    finali=list(np.array(words_nopunct)[togli==0])
+    #LEMMING
+    lemmed_words=[]
+    for w in finali:
+        lemmed_words.append(lem.lemmatize(w,"v"))
+    finali=lemmed_words
+    #ESTRAZIONE DEI BIGRAMMI E LORO DISTRIBUZIONE DI FREQUENZA
+    bigrams = list(nltk.bigrams(finali))
+    fdist = FreqDist(bigrams)
+    ####################ESTRAZIONE DEI BIGRAMMI PIU FREQUENTI
+    freqtot=len(bigrams)*0.02
+    d1=0
+    c1=15
+    while(d1<freqtot):
+        dict2=dict((k, v) for k, v in fdist.items() if v >= c1)
+        d1=sum(dict2.values())
+        c1=c1-1
+    d.append(dict2)
+    
+#contiamo il numero totale di bigrammi estratti    
+p=0
+for i in range(0,len(d)):
+    p=p+len(d[i])
+    
+    
+################################################COSE CHE ABBIAMO DECISO DI NON USARE:
+    #STEMMING
+    #TOGLIERE PAROLE RARE E FREQUENTI TRAMITE DIZIONARIO
+    #TEST D'INDIPENDENZA
 
 # STEMMING:
 # Stemming is a process of linguistic normalization, which reduces words to their word root word or chops off the derivational affixes.
 # For example, connection, connected, connecting word reduce to a common word "connect".
 
 # from nltk.stem import PorterStemmer
-
 #ps = PorterStemmer()
 #stemmed_words=[]
 #for w in finali:
  #   stemmed_words.append(ps.stem(w))
 
-#print("Filtered Sentence:",finali)
-#print("Stemmed Sentence:",stemmed_words)
 
-#LEMMAIZATION:
-# Lemmatization reduces words to their base word, which is linguistically correct lemmas.
-# Lemmatization is usually more sophisticated than stemming.
-# Stemmer works on an individual word without knowledge of the context. For example, The word "better" has "good" as its lemma.
-# This thing will miss by stemming because it requires a dictionary look-up.
-
-#from nltk.stem.wordnet import WordNetLemmatizer
-#nltk.download('wordnet')
-#lem = WordNetLemmatizer()
-
-#lemmed_words=[]
-#for w in finali:
-#    lemmed_words.append(lem.lemmatize(w,"v"))
-
-
-# STRINGA PULITA
-dict1=Counter(finali)
-vv={k: v for k, v in sorted(dict1.items(), key=lambda item: item[1])}
-
-#tramite i comandi che seguono possiamo modellare il nostro dizionario eliminando facilmente elementi
-dict1 = dict((k, v) for k, v in dict1.items() if v >= 10)
-dict2=dict((k, v) for k, v in dict1.items() if v <= 100)#potrebbe essere interessante togliere anche le parole che si ripetono troppo
-dict2
+# TOGLIERE PAROLE TROPPO FREQUENTI O TROPPO RARE TRAMITE L'UTILIZZO DI UN DIZIONARIO
+#dict1=Counter(finali)
+#vv={k: v for k, v in sorted(dict1.items(), key=lambda item: item[1])}
+#dict2=dict((k, v) for k, v in dict1.items() if v <= 100)#potrebbe essere interessante togliere anche le parole che si ripetono troppo
+#dict2
 
 # Rivediamo graficamente ora i risultati dopo la pulizia
 # fdist = FreqDist(dict2) #si puo fare sia su una stringa che su un dizionario
@@ -163,15 +172,13 @@ dict2
 # plt.show()
 # print ("fatto")
 
-# bigrams
-bigrams = list(nltk.bigrams(finali))
 
+#fdist = FreqDist(bigrams)
+#plt.figure(figsize=(20, 10))
+#fdist.plot(100,cumulative=False)
+#plt.savefig( 'myfig.jpg' ) # Comando che salva l'immagine nella cartella TextMining
 
-# fdist = FreqDist(bigrams)
-# plt.figure(figsize=(20, 10))
-# fdist.plot(30,cumulative=False)
-# plt.savefig( 'myfig.jpg' ) # Comando che salva l'immagine nella cartella TextMining
-
+#### TEST D'INDIPENDENZA
 # Raw popularity count is too crude of a measure.
 # We have to find more clever statistics to be able to pick out meaningful phrases easily.
 # For a given pair of words, the method tests two hypotheses on the observed dataset.
@@ -182,60 +189,60 @@ bigrams = list(nltk.bigrams(finali))
 # Are the observed word occurrences in a given text corpus more likely to have been generated from a model where
 # the two words occur independently from one another?
 
-import math
+#import math
 
-def logL(p,k,n):
-    return k * math.log(p) + (n-k)* math.log( 1 - p + 0.00000000001)
+#def logL(p,k,n):
+#    return k * math.log(p) + (n-k)* math.log( 1 - p + 0.00000000001)
 
-from collections import OrderedDict
-unici = list(OrderedDict.fromkeys(bigrams))
+# from collections import OrderedDict
+# unici = list(OrderedDict.fromkeys(bigrams))
+# unici
 
+# def log_likelihood_statistic (p,k,n):
+#     p1 = p[0]
+#     p2 = p[1]
+#     p = p[2]
+#     k1 = k[0]
+#     k2 = k[1]
+#     n1 = n[0]
+#     n2 = n[1]
 
-def log_likelihood_statistic (p,k,n):
-    p1 = p[0]
-    p2 = p[1]
-    p = p[2]
-    k1 = k[0]
-    k2 = k[1]
-    n1 = n[0]
-    n2 = n[1]
-
-    return 2 * ( logL(p1,k1,n1) + logL(p2,k2,n2) - logL(p,k1,n1) -logL(p,k2,n2) )
-
-
-p = np.zeros(3)
-ris = np.zeros(len(unici))
-
-# Vado a calcolare la log-rapporto di verosimiglianza!!
-for i in range(0,len(unici)):
-    k = np.zeros(2)
-    n = np.zeros(2)
-    for h in range(0,len(bigrams)):
-
-            if unici[i][0] == bigrams[h][0]:
-                k[0] = k[0] + 1
-                n[0] = n[0] + 1
-            if unici[i][0] == bigrams[h][1]:
-                n[0] = n[0] + 1
-
-            if unici[i][1] == bigrams[h][1]:
-                k[1] = k[1] + 1
-                n[1] = n[1] + 1
-            if unici[i][1] == bigrams[h][0]:
-                n[1] = n[1] + 1
-
-    p[0] = (k[0] / n[0])
-    p[1] = (k[1] / n[1])
-    p[2] = (k[0] + k[1]) / (n[0] + n[1])
-
-    ris[i] = log_likelihood_statistic(p,k,n)
-
-ris = list(ris)
-log_ordinata= sorted(range(len(ris)), key=lambda k: ris[k])  #OCCHIO AL MENO !
+#     return 2 * ( logL(p1,k1,n1) + logL(p2,k2,n2) - logL(p,k1,n1) -logL(p,k2,n2) )
 
 
-for k in range(0,30):
-  print(unici[log_ordinata[k]])
+# p = np.zeros(3)
+# ris = np.zeros(len(unici))
 
+# # Vado a calcolare la log-rapporto di verosimiglianza!!
+# for i in range(0,len(unici)):
+#     k = np.zeros(2)
+#     n = np.zeros(2)
+#     for h in range(0,len(bigrams)):
+
+#             if unici[i][0] == bigrams[h][0]:
+#                 k[0] = k[0] + 1
+#                 n[0] = n[0] + 1
+#             if unici[i][0] == bigrams[h][1]:
+#                 n[0] = n[0] + 1
+
+#             if unici[i][1] == bigrams[h][1]:
+#                 k[1] = k[1] + 1
+#                 n[1] = n[1] + 1
+#             if unici[i][1] == bigrams[h][0]:
+#                 n[1] = n[1] + 1
+
+#     p[0] = (k[0] / n[0])
+#     p[1] = (k[1] / n[1])
+#     p[2] = (k[0] + k[1]) / (n[0] + n[1])
+
+#     ris[i] = log_likelihood_statistic(p,k,n)
+
+# ris = list(ris)
+# log_ordinata= sorted(range(len(ris)), key=lambda k: ris[k])  #OCCHIO AL MENO !
+
+
+
+# for k in range(0,30):
+#   print(unici[log_ordinata[k]])
 
 
